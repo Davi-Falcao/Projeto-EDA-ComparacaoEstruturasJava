7a08886f580781e4bd62620766c64bf25c2d50e2
EduardoHeroizo <eduardoheroizo1@hotmail.com>
2026-03-04 17:16:53 -0300

    feat: reseta Estruturas

--- src/main/java/Bench/Benchmark.java
+++ src/main/java/Bench/Benchmark.java
@@ -9,24 +9,21 @@ import EstruturasBstAvlPv.Estrutura;
 
 public class Benchmark {
 
-    public static void main(String[] args) {
-        Benchmark bench = new Benchmark();
-        bench.executar();
+    private ArrayList<Estrutura> resetaEstruturas() {
+
+    ArrayList<Estrutura> estruturas = new ArrayList<>();
+
+    estruturas.add(new BST());
+    estruturas.add(new AVL());
+    estruturas.add(new PV());
+
+    return estruturas;
+    
     }
-
-    public void executar() {
-        ArrayList<Estrutura> estruturas = new ArrayList<>();
-
-        estruturas.add(new BST());
-        estruturas.add(new AVL());
-        estruturas.add(new PV());
-
-        for (Estrutura e : estruturas) {
-            executarBenchmark(e);
-        }
-    }
-
+    
+    
     private void executarBenchmark(Estrutura e) {
         System.out.println("Rodando: " + e.getClass().getSimpleName());
     }
+
 }
