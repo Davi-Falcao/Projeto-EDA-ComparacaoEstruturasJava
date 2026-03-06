b0f48cbf99b5932cd04b323833b372ac355ac42b
joarez-filho <joarez1201@gmail.com>
2026-03-05 22:53:15 -0300

    Feat:BenchAbstrato correção

--- src/main/java/dev/ProjetoEDA/bench/BenchAVL.java
+++ src/main/java/dev/ProjetoEDA/bench/BenchAVL.java
@@ -12,23 +12,21 @@ import java.util.stream.Collectors;
 
 import dev.ProjetoEDA.model.AVL;
 
-public class BenchAVL {
+public class BenchAVL extends BenchAbstrato{
 
     public static void run(){
 
         String resultFilePath = "src/main/java/dev/ProjetoEDA/repository/results/result.csv";
 
         try{
-            lerDados();
+            super.lerDados();
 
             try(BufferedWriter writer = new BufferedWriter(new FileWriter(resultFilePath, true))){
                 if (new File(resultFilePath).length() == 0) {
                     writer.write("TamnhoEntrada,Caso,Estrutura,TipoEntrada,TempoExecucao(ns),MemoriaUso(bytes)\n");
                 }
 
-                testsRandom(writer);
-                testsCrescente(writer);
-                testsDecresente(writer);
+                super.test(writer);
             }
 
         }catch(IOException io){
@@ -38,191 +36,45 @@ public class BenchAVL {
         System.out.println("Resultados gravados em: " + resultFilePath);
     }
 
-    private static void testsRandom(BufferedWriter aux) throws IOException{
+    @Override
+     protected void test(BufferedWriter aux) throws IOException{
 
-        for(int i : entradas){
-            run100I0R(i, "random", aux);
-            run50I50R(i, "random", aux);
-            run75I25R(i, "random", aux);
+        String[] ordens = new String[]{"random", "crescente", "decrescente"};
 
-            run50I50R0S(i, "random", aux);   
-            run50I25R25S(i, "random", aux);  
-            run50I0R50S(i, "random", aux); 
-        }
+        for(String orden : ordens){
 
-    }
-
- 
-    private static void testsCrescente(BufferedWriter aux) throws IOException{
-        for(int i : entradas){
-            run100I0R(i, "crescente", aux);
-            run50I50R(i, "crescente", aux);
-            run75I25R(i, "crescente", aux);
-
-            run50I50R0S(i, "crescente", aux);
-            run50I25R25S(i, "crescente", aux);
-            run50I0R50S(i, "crescente", aux);
-        }
-    }
-
-   
-    private static void testsDecresente(BufferedWriter aux) throws IOException{
-        for(int i : entradas){
-            run100I0R(i, "decresente", aux);
-            run50I50R(i, "decresente", aux);
-            run75I25R(i, "decresente", aux);
-
-            run50I50R0S(i, "decresente", aux);
-            run50I25R25S(i, "decresente", aux);
-            run50I0R50S(i, "decresente", aux);
-        }
-    }
-
-    private static void run100I0R(int entrada, String tipo, BufferedWriter writer){
-        experimento(entrada, "100I0R", writer, tipo);
-    }
-
-    private static void run50I50R(int entrada, String tipo, BufferedWriter writer){
-        experimento(entrada, "50I50R", writer, tipo);
-    }
-
-    private static void run75I25R(int entrada, String tipo, BufferedWriter writer){
-        experimento(entrada, "75I25R", writer, tipo);
-    }
-
-    private static void run50I50R0S(int entrada, String tipo, BufferedWriter writer){
-        experimento(entrada, "50I50R0S", writer, tipo);
-    }
-
-    private static void run50I25R25S(int entrada, String tipo, BufferedWriter writer){
-        experimento(entrada, "50I25R25S", writer, tipo);
-    }
-
-    private static void run50I0R50S(int entrada, String tipo, BufferedWriter writer){
-        experimento(entrada, "50I0R50S", writer, tipo);
-    }
-
-    private static void experimento(int entrada, String test, BufferedWriter writer, String tipo){
-        List<Integer> dados = null;
-
-        switch (tipo) {
-            case "random":
-                dados = random;
-                break;
-            case "crescente":
-                dados = crescente;
-                break;
-            default:
-                dados = decresente;
-                break;
-        }
-
-        long[] tempos = new long[REPETICOES];
-        long[] memorias = new long[REPETICOES];
-
-        for (int s = 0; s < REPETICOES; s++) {
-            long memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-            long tempoAntes = System.nanoTime();
-
-            AVL avl = new AVL();
-
-            switch (test) {
-
-                case "100I0R":
-                    for(int i = 0; i < entrada; i ++) avl.add(dados.get(i));
-                    break;
-
-                case "50I50R": {
-                    int ins = (int)(entrada * 0.5);
-                    for(int i = 0; i < ins; i ++) avl.add(dados.get(i));
-                    for(int i = 0; i < ins; i++) avl.remove(dados.get(i));
-                    break;
-                }
-
-                case "75I25R": {
-                    int ins = (int)(entrada * 0.75);
-                    int rem = (int)(entrada * 0.25);
-                    for(int i = 0; i < ins; i ++) avl.add(dados.get(i));
-                    for(int i = 0; i < rem; i++) avl.remove(dados.get(i));
-                    break;
-                }
-
-                case "50I50R0S": {
-                    int ins = (int)(entrada * 0.5);
-                    int rem = (int)(entrada * 0.5);
-
-                    for(int i = 0; i < ins; i++) avl.add(dados.get(i));
-                    for(int i = 0; i < rem; i++) avl.remove(dados.get(i));
-                    break;
-                }
-
-                case "50I25R25S": {
-                    int ins = (int)(entrada * 0.5);
-                    int rem = (int)(entrada * 0.25);
-                    int sea = (int)(entrada * 0.25);
-
-                    for(int i = 0; i < ins; i++) avl.add(dados.get(i));
-                    for(int i = 0; i < rem; i++) avl.remove(dados.get(i));
-
-                    for(int i = 0; i < sea; i++) avl.search(dados.get(rem + i));
-                    break;
-                }
-
-                case "50I0R50S": {
-                    int ins = (int)(entrada * 0.5);
-                    int sea = (int)(entrada * 0.5);
-
-                    for(int i = 0; i < ins; i++) avl.add(dados.get(i));
-
-                    for(int i = 0; i < sea; i++) avl.search(dados.get(i));
-                    break;
-                }
+            for(int i : entradas){
+                run100I0R(i, orden, aux);
+                run50I50R(i, orden, aux);
+                run75I25R(i,orden, aux);
             }
-
-            long memoriaDepois = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-            long tempoDepois = System.nanoTime();
-
-            tempos[s] = tempoDepois - tempoAntes;
-            memorias[s] = memoriaDepois - memoriaAntes;
-        }
-
-        long tempoMediana = calcularMediana(tempos);
-        long memoriaMediana = calcularMediana(memorias);
-
-        try{
-            writer.write(
-                entrada + "," +
-                test + "," +
-                "AVL" + "," +
-                tipo + "," +
-                tempoMediana + "," +
-                memoriaMediana + "\n"
-            );
-        }catch(IOException io){
-            io.printStackTrace();
-        }
     }
 
-    private static long calcularMediana(long[] valores) {
-        Arrays.sort(valores);
+    }   
 
-        int n = valores.length;
-        if (n % 2 == 1) {
-            return valores[n / 2];
-        } else {
-            return (valores[n / 2 - 1] + valores[n / 2]) / 2;
-        }
+    private void run100I0R(int entrada, String tipo, BufferedWriter writer){
+        experimento(entrada, "AVL", "100I0R", writer, tipo);
     }
 
-    private static void lerDados() throws IOException{
-        String caminhoRandom = "src/main/java/dev/ProjetoEDA/repository/entry/entradaRandomUnica.csv";
-        String caminhoCrescente = "src/main/java/dev/ProjetoEDA/repository/entry/entradaCrescenteUnica.csv";
-        String caminhoDecresente = "src/main/java/dev/ProjetoEDA/repository/entry/entradaDecrescenteUnica.csv";
-        String caminhoentradas = "src/main/java/dev/ProjetoEDA/repository/entry/tamanhoEntrada.csv";
-
-        random = Files.lines(Paths.get(caminhoRandom)).map(Integer::valueOf).collect(Collectors.toList());
-        crescente = Files.lines(Paths.get(caminhoCrescente)).map(Integer::valueOf).collect(Collectors.toList());
-        decresente = Files.lines(Paths.get(caminhoDecresente)).map(Integer::valueOf).collect(Collectors.toList());
-        entradas = Files.lines(Paths.get(caminhoentradas)).map(Integer::valueOf).collect(Collectors.toList());
+    private  void run50I50R(int entrada, String tipo, BufferedWriter writer){
+        experimento(entrada,"AVL", "50I50R", writer, tipo);
     }
+
+    private void run75I25R(int entrada, String tipo, BufferedWriter writer){
+        experimento(entrada,"AVL", "75I25R", writer, tipo);
+    }
+
+    private void run50I50R0S(int entrada, String tipo, BufferedWriter writer){
+        experimento(entrada,"AVL", "50I50R0S", writer, tipo);
+    }
+
+    private void run50I25R25S(int entrada, String tipo, BufferedWriter writer){
+        experimento(entrada,"AVL", "50I25R25S", writer, tipo);
+    }
+
+    private void run50I0R50S(int entrada, String tipo, BufferedWriter writer){
+        experimento(entrada,"AVL", "50I0R50S", writer, tipo);
+    }
+
+
 }
--- src/main/java/dev/ProjetoEDA/bench/BenchAbstrato.java
+++ src/main/java/dev/ProjetoEDA/bench/BenchAbstrato.java
@@ -1,3 +1,14 @@
+import java.util.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public abstract class BenchAbstrato{
 
     protected static List<Integer> random;
@@ -8,6 +19,85 @@ public abstract class BenchAbstrato{
     protected static final int REPETICOES = 18;
 
     public abstract void run();
+
+    protected abstract void test(BufferedWriter aux) throws IOException;
+
+    protected void experimento(int entrada, String estrutura ,String test, BufferedWriter writer, String tipo){
+        List<Integer> dados = super.getDados();
+
+        long[] tempos = new long[REPETICOES];
+        long[] memorias = new long[REPETICOES];
+
+        for (int s = 0; s < REPETICOES; s++) {
+            long memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            long tempoAntes = System.nanoTime();
+
+            executaCaso(dados, entrada, test);
+
+            long memoriaDepois = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            long tempoDepois = System.nanoTime();
+
+            tempos[s] = tempoDepois - tempoAntes;
+            memorias[s] = memoriaDepois - memoriaAntes;
+        }
+
+        long tempoMediana = calcularMediana(tempos);
+        long memoriaMediana = calcularMediana(memorias);
+
+        try{
+            writer.write(
+                entrada + "," +
+                test + "," +
+                estrutura + "," +
+                tipo + "," +
+                tempoMediana + "," +
+                memoriaMediana + "\n"
+            );
+        }catch(IOException io){
+            io.printStackTrace();
+        }
+    }
+
+    protected executaCaso(List<Integer> dados, int entrada, String test){
+            switch (test) {
+
+                case "100I0R":
+                    executarI100_R0_S0(dados, entrada);
+                    break;
+
+                case "50I50R0S": {
+                    executarI50_R50_S0(dados, entrada);
+                    break;
+                }
+
+                case "75I25R": {
+                    executarI75_R25_S0(dados, entrada);
+                    break;
+                }
+
+                case "50I25R25S": {
+                    executarI50_R25_S25(dados, entrada);
+                }
+
+                case "50I0R50S": {
+                    executarI50_R0_S50(dados, entrada);
+                    break;
+                }default:
+                    break;
+        }
+    }
+
+    protected abstract void executarI100_R0_S0(List<Integer> dados, int n);
+    protected abstract void executarI50_R50_S0(List<Integer> dados, int n);
+    protected abstract void executarI75_R25_S0(List<Integer> dados, int n);
+    protected abstract void executarI50_R25_S25(List<Integer> dados, int n);
+    protected abstract void executarI50_R0_S50(List<Integer> dados, int n);
+
+    protected List<Integer> getDados(String dado){
+        if(dado.equals("random")) return random;
+        else if(dado.equals("crescente")) return crescente;
+        return decresente;
+    }
 
     protected   long calcularMediana(long[] valores) {
         Arrayssort(valores);
@@ -20,7 +110,7 @@ public abstract class BenchAbstrato{
         }
     }
 
-    protected static void lerDados() throws IOException{
+    protected void lerDados() throws IOException{
         String caminhoRandom = "src/main/java/dev/ProjetoEDA/repository/entry/entradaRandomUnica.csv";
         String caminhoCrescente = "src/main/java/dev/ProjetoEDA/repository/entry/entradaCrescenteUnica.csv";
         String caminhoDecresente = "src/main/java/dev/ProjetoEDA/repository/entry/entradaDecrescenteUnica.csv";
@@ -31,3 +121,4 @@ public abstract class BenchAbstrato{
         decresente = Files.lines(Paths.get(caminhoDecresente)).map(Integer::valueOf).collect(Collectors.toList());
         entradas = Files.lines(Paths.get(caminhoentradas)).map(Integer::valueOf).collect(Collectors.toList());
     }
+}
